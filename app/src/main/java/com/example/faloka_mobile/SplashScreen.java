76f9369e1e03b1825563bcb58865a6b901b31529
aguskertha <baguskertha@gmail.com>
2021-09-17 19:27:49 +0700

    faloka-android-6 Design Splash Screen + Implement in Activity

---
+++ app/src/main/java/com/example/faloka_mobile/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.faloka_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L);
+    }
+}
