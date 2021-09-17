2cfc9279a0c50ba71478020c34d3d71ae480c8dc
aguskertha <baguskertha@gmail.com>
2021-09-18 02:00:03 +0700

    faloka-android-7 Design UI Register - Aseet Icon Register - Font - String
    closes #7

---
+++ app/src/main/java/com/example/faloka_mobile/Register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.faloka_mobile.Register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.faloka_mobile.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register );
+    }
+}
