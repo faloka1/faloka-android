6cbaa23ef25b8e6e97edb20e02c2b96af3188c8e
aguskertha <baguskertha@gmail.com>
2021-09-20 19:13:28 +0700

    faloka-android-15 HomePage AppBar, Navigation Bottom, PageLayout - Asset Icon - Toolbar + Item (Wishlist & Cart) - Bottom Nav + Item (Home, Search, Inspire, Account)
    closes #15

--- app/src/main/java/com/example/faloka_mobile/SplashScreen.java
+++ app/src/main/java/com/example/faloka_mobile/SplashScreen.java
@@ -22,7 +22,7 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
         }, 2000L);
