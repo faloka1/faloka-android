d5c8b7af8577db4153f516cf699a05b61d437baa
aguskertha <baguskertha@gmail.com>
2021-10-01 14:08:59 +0700

    faloka-android-33 Integration API List Product to Activity
    - Done Add Action Bar Product List

--- app/src/main/java/com/example/faloka_mobile/Product_List/ProductListActivity.java
+++ app/src/main/java/com/example/faloka_mobile/Product_List/ProductListActivity.java
@@ -1,12 +1,19 @@
 package com.example.faloka_mobile.Product_List;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.faloka_mobile.Home.HomeFragment;
 import com.example.faloka_mobile.R;
@@ -19,6 +26,13 @@ public class ProductListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_product_list);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setTitle("Blouse");
 
         ArrayList<ProductResponse> products = new ArrayList<>();
         products.add(new ProductResponse("Blouse bagus",R.drawable.product_image,"Toko bagus",8000));
@@ -37,4 +51,29 @@ public class ProductListActivity extends AppCompatActivity {
         rvStyleInspiration.setAdapter(productAdapter);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+            case R.id.top_menu_wishlist:
+                Toast.makeText(getApplicationContext(), "WISHLIST", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.top_menu_cart:
+                Toast.makeText(getApplicationContext(), "CART", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
