a8b9aafc6d611fd19a4d513fb749d02fb3dba2a5
elaamr7 <elaamr7@gmail.com>
2021-10-15 00:01:31 +0700

    deleting old files before adding updated project

--- app/src/main/java/com/example/faloka_mobile/Checkout/PaymentMethodSelectedListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.faloka_mobile.Checkout;
-
-import com.example.faloka_mobile.Model.PaymentMethod;
-
-public interface PaymentMethodSelectedListener {
-    void onPaymentMethodSelected(PaymentMethod paymentMethod);
-}
