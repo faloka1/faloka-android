1cc5ce615dbc9ea3d8fe22c85e3c91cdce7d7c19
elaamr7 <elaamr7@gmail.com>
2021-10-22 07:37:27 +0700

    faloka-android-74 Slicing profile

--- app/src/main/java/com/example/faloka_mobile/Account/AccountFragment.java
+++ app/src/main/java/com/example/faloka_mobile/Account/AccountFragment.java
@@ -1,25 +1,134 @@
 package com.example.faloka_mobile.Account;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.faloka_mobile.R;
+import com.example.faloka_mobile.databinding.FragmentAccountBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private FragmentAccountBinding binding;
+    private DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public  AppCompatActivity activity;
+
+    private String[] titles = new String[]{"Movies", "Events", "Tickets"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = FragmentAccountBinding.inflate(getLayoutInflater());
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        activity = ((AppCompatActivity)getActivity());
+        createTab(view);
+        createDrawer(view);
+        activity.setSupportActionBar(view.findViewById(R.id.account_toolbar));
+
+        return view;
     }
+    private void createDrawer(View view){
+        drawerLayout = view.findViewById(R.id.account_drawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public  void createTab(View view){
+
+        tabLayout = view.findViewById(R.id.account_main_tab);
+        viewPager = view.findViewById(R.id.account_content_view_pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Pesanan Saya"));
+        tabLayout.addTab(tabLayout.newTab().setText("Postingan"));
+
+        final MyAdapter adapter = new MyAdapter(getContext(),getActivity().getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    public class MyAdapter extends FragmentPagerAdapter {
+
+        private Context myContext;
+        int totalTabs;
+
+        public MyAdapter(Context context, FragmentManager fm, int totalTabs) {
+            super(fm);
+            myContext = context;
+            this.totalTabs = totalTabs;
+        }
+
+        // this is for fragment tabs
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new OrderFragment();
+                case 1:
+                    return new PostFragment();
+                default:
+                    return null;
+            }
+        }
+        @Override
+        public int getCount() {
+            return totalTabs;
+        }
+    }
+
 }
