6be1a03827bd8f4e6af1b2949fb5a386b2230bf0
elaamr7 <elaamr7@gmail.com>
2021-10-25 16:41:37 +0700

    Slicing form upload inspire me

---
+++ app/src/main/java/com/example/faloka_mobile/InspireMe/InspireMeFormUploadActivity.java
@@ -0,0 +1,16 @@
+package com.example.faloka_mobile.InspireMe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.faloka_mobile.R;
+
+public class InspireMeFormUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inspire_me_form_upload);
+    }
+}
