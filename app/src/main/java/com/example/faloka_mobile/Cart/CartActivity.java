ea46db3a3ba2801e6a65baed7b13ebc94ec97beb
aguskertha <baguskertha@gmail.com>
2021-10-29 13:44:34 +0700

    faloka-android-94 Cart Activity
    - slicing done i think

---
+++ app/src/main/java/com/example/faloka_mobile/Cart/CartActivity.java
@@ -0,0 +1,16 @@
+package com.example.faloka_mobile.Cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.faloka_mobile.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
