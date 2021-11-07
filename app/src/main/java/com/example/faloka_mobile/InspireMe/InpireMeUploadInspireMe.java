06ab78096a3f80ff646f6f649c001c27e7a35ebb
elaamr7 <elaamr7@gmail.com>
2021-11-07 21:38:06 +0700

    faloka-android-85 Slicing inspire me

---
+++ app/src/main/java/com/example/faloka_mobile/InspireMe/InpireMeUploadInspireMe.java
@@ -0,0 +1,16 @@
+package com.example.faloka_mobile.InspireMe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.faloka_mobile.R;
+
+public class InpireMeUploadInspireMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inpire_me_upload_inspire_me);
+    }
+}
